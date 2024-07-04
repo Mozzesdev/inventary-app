f6f7fd2f02f677fa844503019b9855ee9ae2bdd8
Moisés Zambrano <moiseszambrano488@gmail.com>
2024-07-04 15:50:47 +0200

    commit

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import compression from "compression";
-import { root } from "./server/root.js";
 import apiRouter from "./server/api/router.js";
 import cookieParser from "cookie-parser";
 import { NODE_ENV, PORT } from "./server/config.js";
@@ -8,6 +7,12 @@ import redis from "./server/redis/config.js";
 import { corsMiddleware } from "./server/middlewares/cors.middleware.js";
 import { userMiddleware } from "./server/middlewares/user.middleware.js";
 import renderMiddleware from "./server/middlewares/render.middleware.js";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const root = __dirname;
 
 const app = express();
 app.disable("x-powered-by");
